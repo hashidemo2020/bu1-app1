80200adb629eb5c8ad059965e7a7e1fd381c05fb
Prakash Manglanathan <pmnathan@gmail.com>
2020-02-13 16:06:01 -0500

    Update 1-config.tf

--- services/1-config.tf
+++ services/1-config.tf
@@ -5,3 +5,15 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+
+
+data "terraform_remote_state" "ecc_view" {
+  backend = "remote"
+  config = {
+    organization = var.remote_organization
+    workspaces = {
+      name = var.view_remote_workspace_name
+    }
+  }
+}
